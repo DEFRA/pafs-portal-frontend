80534831df7b389b97e078aa72bbadb25aa66bf4
Fujale Ahmed Shaik <795110@cognizant.com>
2025-11-20 23:24:09 +0000

    634478-641207: No magic number

--- src/server/common/constants/session.js
+++ src/server/common/constants/session.js
@@ -1,7 +1,7 @@
 export const SESSION = {
   REFRESH_BUFFER_MS: 60000, // Refresh 1 minute before expiry
   INACTIVE_TIMEOUT_MS: 1800000, // 30 minutes
-  TOKEN_REFRESH_MS: 15 * 60 * 1000, // Default 15 minutes
+  TOKEN_REFRESH_MS: 900000, // Default 15 minutes
 
   SESSION_TIMEOUT: 'SESSION_TIMEOUT'
 }
