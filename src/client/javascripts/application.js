868635ded1ad67e3de024a3368fa96bb260bc7c0
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2026-03-03 18:33:31 +0000

    664064_640368_640370_630622: Fixing the sonarqube issue

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -20,6 +20,7 @@ createAll(ServiceNavigation)
 createAll(SkipLink)
 
 // Custom header navigation toggle for mobile (v5-style navigation in v6)
+const ARIA_EXPANDED = 'aria-expanded'
 const headerToggleButton = document.querySelector('.govuk-js-header-toggle')
 if (headerToggleButton) {
   const navigationWrapper = document.querySelector(
@@ -27,11 +28,11 @@ if (headerToggleButton) {
   )
 
   headerToggleButton.removeAttribute('hidden')
-  headerToggleButton.setAttribute('aria-expanded', 'false')
+  headerToggleButton.setAttribute(ARIA_EXPANDED, 'false')
 
   headerToggleButton.addEventListener('click', function () {
-    const isOpen = this.getAttribute('aria-expanded') === 'true'
-    this.setAttribute('aria-expanded', isOpen ? 'false' : 'true')
+    const isOpen = this.getAttribute(ARIA_EXPANDED) === 'true'
+    this.setAttribute(ARIA_EXPANDED, isOpen ? 'false' : 'true')
 
     if (navigationWrapper) {
       navigationWrapper.classList.toggle(
