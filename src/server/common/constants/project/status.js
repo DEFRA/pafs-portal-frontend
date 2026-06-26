de28b9fceaf95b5122e4c6a624b069c12343d1f9
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2026-06-26 08:25:29 +0100

    Fixing the incorrect download stats - becuase of data quality issue

--- src/server/common/constants/project/status.js
+++ src/server/common/constants/project/status.js
@@ -4,7 +4,8 @@ export const PROJECT_STATUS = {
   SUBMITTED: 'submitted',
   APPROVED: 'approved',
   REJECTED: 'rejected',
-  ARCHIVED: 'archived'
+  ARCHIVED: 'archived',
+  COMPLETED: 'completed'
 }
 
 export const EDITABLE_STATUSES = [PROJECT_STATUS.DRAFT, PROJECT_STATUS.REVISE]
